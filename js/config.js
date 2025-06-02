c72295b3f83711d14152bdf2856883ccda781e18
astrologiaindiana <astrologiaindianabr@gmail.com>
2025-06-02 18:24:10 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,5 +1,3 @@
-// config.js
-
 // Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyAngAB_zoXr5lsi1N8WocVJeHFq6QjdUUs",
@@ -10,18 +8,18 @@ const firebaseConfig = {
   appId: "1:1055729827966:web:51954b0cabee762653d82f"
 };
 
+// Inicializar Firebase
 firebase.initializeApp(firebaseConfig);
 
-// Airtable
+// Airtable (usa biblioteca: https://cdn.jsdelivr.net/npm/airtable@0.12.2/build/airtable.browser.js)
 window.airtableBase = new Airtable({
   apiKey: "patkcHF16ytjQFYtf.2d2b97aeab44b5961a1c7e4c68e6f5e2bdef0b81f2cd0303dc2580f9d96df10d"
 }).base("appc74NoitSC8w1XQ");
 
+// Tabelas
 window.TABLES = {
   MAP_TYPES: "Tipos de Mapa",
   ORDERS: "Pedidos",
   CLIENTS: "Clientes",
-  VIDEO_CALLS: "Videochamada" // Corrigido aqui
-};
-
+  VIDEO_CALLS: "Videochamada" // Corrigido (sem "s")
 };
