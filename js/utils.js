c464df24ec1ad33da18693d2a0a890e7ad31cf1b
astrologiaindiana <astrologiaindianabr@gmail.com>
2025-06-02 17:12:19 -0300

    Update utils.js

--- js/utils.js
+++ js/utils.js
@@ -202,13 +202,13 @@ function exportTableToCSV(tableId, filename) {
 }
 
 // Registrar service worker para PWA
-    if ('serviceWorker' in navigator) {
-        navigator.serviceWorker.register('/service-worker.js')
-            .then(registration => {
-                console.log('Service Worker registrado com sucesso:', registration);
-            })
-            .catch(error => {
-                console.error('Erro ao registrar Service Worker:', error);
-            });
-    }
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(registration => {
+            console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch(error => {
+            console.error('Erro ao registrar Service Worker:', error);
+        });
 }
+
